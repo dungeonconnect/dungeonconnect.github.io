b8180d270ee993bc1e4c53709b0165b98a5a30f8
lcbrady <lcbrady@ncsu.edu>
2023-01-06 00:18:46 -0500

    [Implement]
    Added a dummy div to ensure that the hud never covers anything up

--- nav/nav-animate.js
+++ nav/nav-animate.js
@@ -1,15 +1,18 @@
-function toggleNav(id, id2) {
+function toggleNav(id, id2, id3) {
 		var x = document.getElementById(id);
 		var y = document.getElementById(id2);
+		var z = document.getElementById(id3);
 		const notPhone = window.matchMedia('(min-width: 768px)');
 		if (notPhone.matches) {
 			if (x.style.display === "none") {
 				x.style.display = null;
+				z.style.display = null;
 				showNav(x);
 			} else {
 				hideNav(x);
 				sleep(220).then(() => {
 					x.style.display = "none";
+					z.style.display = "none";
 				});
 			}
 		}
@@ -25,11 +28,11 @@ function toggleNav(id, id2) {
 			}
 		}
 	}
-	
+
 	function sleep(ms) {
 		  return new Promise((resolve) => setTimeout(resolve, ms));
 		}
-	
+
 	function hideNav(x) {
 		const hudHide = [
 			{transform: 'translate(0px, 0px)'},
@@ -45,7 +48,7 @@ function toggleNav(id, id2) {
 				}
 		x.animate(hudHide, timing);
 	}
-	
+
 	function showNav(x) {
 		const hudHide = [
 			{transform: 'translate(0px, -50px)'},
@@ -61,7 +64,7 @@ function toggleNav(id, id2) {
 				}
 		x.animate(hudHide, timing);
 	}
-	
+
 	function hideMini(x) {
 		const hudHide = [
 			{transform: 'translate(0px, 0px)'},
@@ -73,7 +76,7 @@ function toggleNav(id, id2) {
 				}
 		x.animate(hudHide, timing);
 	}
-	
+
 	function showMini(x) {
 		const hudHide = [
 			{transform: 'translate(220px, 0px)'},
@@ -85,12 +88,12 @@ function toggleNav(id, id2) {
 				}
 		x.animate(hudHide, timing);
 	}
-	
+
 	function toggleSub(id) {
 		var x = document.getElementById(id);
 		if (x.style.display === "none") {
 			x.style.display = null;
-			
+
 		} else {
 			x.style.display = "none";
 		}
